075142ed717db01d7c600c2a11118a8722d4c89f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-24 21:00:20 -0300

    feat: add files

---
+++ src/test/java/com/github/FreitasRj1/study_apiw/StudyApiwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.FreitasRj1.study_apiw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
